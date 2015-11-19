4f2af13e04ad8007a2b420f67f33f66d8744da19
chackotaco <mikechacko10@gmail.com>
2015-11-19 16:01:41 -0600

    Added file liser

--- main.c
+++ main.c
@@ -4,24 +4,34 @@
  *  Created on: Nov 16, 2015
  */
 
+#include <sys/types.h>
+#include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
+int main( int argc, const char* argv[] )
+{
+	int i = 0;
+	struct dirent *ep;
+	char *path;
+	char *fileArray;
+	path = (char *)argv[1];
+	
+    DIR *dir;
 
-int main(int argc, char *argv[]){
+    dir = opendir(path);
 	
-	if( argc > 2 ){
-      printf("Too many arguments supplied.\n");
+    if(dir != NULL){
+		i = 0;
+		while(ep = readdir(dir)){
+       		printf("%s\n", ep->d_name);
+			
+			fileArray[i] = ep->d_name;
+			i++;
+		}
+        closedir(dir);
     }
-    else{
-      printf("One argument expected.\n");
+    else
+    { 
+		perror("Cannot open directory \n");
     }
-	
-
-	if (ENOENT == errno) {
-		printf("Invalid Directory");
-	} 
-	else{
-		printf("passed");
-	}
-	
+	return 0;
 }
