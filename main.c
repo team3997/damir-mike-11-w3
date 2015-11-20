0d1c8c0b1609c0b02b974a17f62bd7268185b055
Michael Chacko <mikechacko10@gmail.com>
2015-11-19 20:57:38 -0600

    Directory listing is now put into two dimensional array and prints to text file

--- main.c
+++ main.c
@@ -8,10 +8,13 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main( int argc, const char* argv[] )
 {
 	int i = 0;
+	int n = 0;
 	struct dirent *ep;
 	DIR *dir;
 
@@ -20,15 +23,24 @@ int main( int argc, const char* argv[] )
 
 	FILE *textFile = fopen("listing.txt", "w");
 	
-
-
+	char **items = NULL;
 	
     	dir = opendir(path);
     	if(dir != NULL){
 		while(ep = readdir(dir)){
-       			puts(ep->d_name);	
-			fprintf(textFile, "%s\n", ep->d_name);
+			if(strcmp(ep->d_name, ".") == 0 || strcmp(ep->d_name, "..") == 0)
+				continue;
+			printf("%s\n", ep->d_name);
+			items = (char**) realloc(items, sizeof(char*) * (i+1));
+			items[i] = (char*) realloc(items[i], strlen(ep->d_name));
+			strcpy(items[i], ep->d_name);
+			i++;
 		}
+
+		for(n = 0; n < i; n++){
+			fprintf(textFile, "%s\n", items[n]);
+		}
+
         	(void) closedir(dir);
 		fclose(textFile);
     	}
