c249ce8d0d982e97c94e143f9f1dbea551b024b4
Damir <dmrgluhak@gmail.com>
2015-11-19 19:57:23 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 16, 2015
+ */
+
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 int main( int argc, const char* argv[] )
 {
-   DIR *opendir(argv[1] *directory);
+	int i = 0;
+	struct dirent *ep;
+	DIR *dir;
+
+	char *path;
+	path = (char *)argv[1];
+
+	FILE *textFile = fopen("listing.txt", "w");
+	
+
+
+	
+    	dir = opendir(path);
+    	if(dir != NULL){
+		while(ep = readdir(dir)){
+       			puts(ep->d_name);	
+			fprintf(textFile, "%s\n", ep->d_name);
+		}
+        	(void) closedir(dir);
+		fclose(textFile);
+    	}
+    	else
+    	{ 
+		perror("Cannot open directory \n");
+    	}
+	return 0;
 }
